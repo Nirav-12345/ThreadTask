daf4845a21d0678ed418bd7525f5d50e2fbd5030
Nirav-12345 <niravpandit0002@gmail.com>
2022-04-02 21:54:55 +0530

    adding addresbook and thread parallel thead

---
+++ AddressbookThread/AdBook.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressbookThread
+{
+    public class AdBook
+    {
+        public int EmpId { get; set; }
+
+
+
+
+        public string empName { get; set; }
+
+
+        
+
+
+        public string Address { get; set; }
+
+
+        public string city { get; set; }
+
+
+        public string state { get; set; }
+
+
+        public string Zip { get; set; }
+
+        public string email { get; set; }
+
+        public double contact { get; set; }
+
+
+        public AdBook(int empid, string EmpName, string address, string City, string State, string zip, string Email, double Contact )
+        {
+            this.EmpId = empid;
+            this.empName = EmpName;
+            this.Address = address;
+            this.city = City;
+            this.state = State;
+            this.Zip = zip;
+            this.email = Email;
+            this.contact = Contact;
+        }
+    }
+}
---
+++ AddressbookThread/Adressbookopreation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressbookThread
+{
+    public class Adressbookopreation
+    {
+        List<AdBook> listperson = new List<AdBook>();
+
+        public void addressRoll(List<AdBook> ad)
+        {
+            ad.ForEach(addressData =>
+            {
+                Console.WriteLine("Employee being added:" + addressData.empName);
+                this.addressRoll(addressData);
+                Console.WriteLine("Employee added: " + addressData.empName);
+            });
+            Console.WriteLine(this.listperson.ToString());
+        }
+        private void addressRoll(AdBook address)
+        {
+            listperson.Add(address);
+
+        }
+
+
+
+
+        public void addressRollThread(List<AdBook> ad)
+        {
+            ad.ForEach(addressData =>
+            {
+
+                Task tread = new Task(() =>
+                {
+
+                    Console.WriteLine("Employee being added:" + addressData.empName);
+                    this.addressRoll(addressData);
+                    Console.WriteLine("Employee added: " + addressData.empName);
+                });
+                tread.Start();
+
+
+
+            });
+            Console.WriteLine(this.listperson.ToString());
+        }
+
+    }
+}
