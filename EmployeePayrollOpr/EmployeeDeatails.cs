28c56f26d18771626227ea362850f44970debb0f
Nirav-12345 <niravpandit0002@gmail.com>
2022-04-02 21:52:42 +0530

    adding employee thread and prallel thread

---
+++ EmployeePayrollOpr/EmployeeDeatails.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollOpr
+{
+    public class EmployeeDeatails
+    {
+        public int EmployeeID { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string Phonenumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string Department { get; set; }
+
+        public char Gender { get; set; }
+
+        public double BasicPay { get; set; }
+
+        public double Deduction { get; set; }
+
+        public double TaxablePay { get; set; }
+
+        public double Tax { get; set; }
+
+        public string city { get; set; }
+
+        public string Country { get; set; }
+
+        public EmployeeDeatails(int employeeid,string employeename,string phonenumber,string address,string department,char gender,double basicpay,double deduction,double taxablepay,double tax,string City,string country)
+        {
+            this.EmployeeID = employeeid;
+            this.EmployeeName = employeename;
+            this.Phonenumber = phonenumber;
+            this.Address = address;
+            this.Department = department;
+            this.Gender = gender;
+            this.BasicPay = basicpay;
+            this.Deduction = deduction;
+            this.TaxablePay = taxablepay;
+            this.Tax = tax;
+            this.city = City;
+            this.Country = country;
+        }
+             
+
+
+
+    }
+}
